45e284bc3d3e58ef6d507cd58c067415633897ea
Holly Christensen <holly.christensen@comcast.net>
2021-11-24 23:30:08 -0500

    cleanup & styling with themes

--- src/index.tsx
+++ src/index.tsx
@@ -15,68 +15,46 @@ import TextImageSection, {
 import ImageGallery, { ImageGalleryProps } from "./layouts/ImageGallery";
 import ImageFull, { ImageFullProps } from "./layouts/ImageFull";
 import TypographyPage from "./pages/type";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
 
-const postHeaderProps: PostHeaderProps = {
-  post: {
-    title: "Title",
-    imgPath: image1,
-    imgAlt: "alt text!",
-    role: "role!",
-    what: "what",
-    when: "when",
-    tools: "toolz",
-    linkText: "a link!",
-    linkUrl: "#",
-    about:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante enim, pulvinar et massa et, pulvinar finibus erat. Maecenas porttitor pellentesque ante non tempus. Aenean nulla odio, efficitur a nisl ut, viverra posuere lectus. Quisque pulvinar dictum justo quis dignissim. Donec sit amet nulla accumsan, laoreet odio vitae, tristique erat. Pellentesque euismod sodales mollis. Sed rutrum congue pretium. Vestibulum pharetra convallis nisl. Duis a urna luctus, tempus sapien non, bibendum erat. Suspendisse massa velit, molestie non consectetur id, rutrum et metus. Integer pellentesque condimentum nisl, quis pulvinar massa rhoncus sed. Sed in posuere nulla, ut sodales mauris. In auctor nulla in velit iaculis, ac gravida nisl rhoncus. Donec nec porttitor nisl. Sed quis nibh elementum, maximus tortor eget, convallis est. Sed in posuere nulla, ut sodales mauris. In auctor nulla in velit iaculis, ac gravida nisl rhoncus. Donec nec porttitor nisl. Sed quis nibh elementum, maximus tortor eget, convallis.",
+const theme = createTheme({
+  typography: {
+    h3: {
+      fontSize: 40,
+      fontFamily: [
+        "Monotalic",
+        "Karla",
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+      ].join(","),
+    },
+    h4: {
+      marginBottom: 15,
+    },
+    h5: {
+      fontSize: 17,
+    },
+    fontFamily: ["Open Sans", '"Helvetica Neue"', "Arial", "sans-serif"].join(
+      ","
+    ),
+    fontSize: 11,
   },
-};
-const textSectionProps: TextSectionProps = {
-  post: {
-    header: "Section title",
-    subheaders: ["header1", "header2", "header3"],
-    bodies: [
-      "Integer pellentesque condimentum nisl, quis pulvinar massa rhoncus sed.",
-      "Sed quis nibh elementum, maximus tortor eget, convallis est. Sed in posuere nulla, ut sodales mauris.",
-      "Quisque pulvinar dictum justo quis dignissim. Donec sit amet nulla accumsan, laoreet odio vitae, tristique erat. Pellentesque euismod sodales mollis.",
-    ],
-  },
-};
-const textImageSectionProps: TextImageSectionProps = {
-  post: {
-    imgPath: image1,
-    imgAlt: "alt text!",
-    caption: "this is a caption!!",
-    header: "Section title",
-    body: "Integer pellentesque condimentum nisl, quis pulvinar massa rhoncus sed. Sed quis nibh elementum, maximus tortor eget, convallis est. Sed in posuere nulla, ut sodales mauris. Quisque pulvinar dictum justo quis dignissim. Donec sit amet nulla accumsan, laoreet odio vitae, tristique erat. Pellentesque euismod sodales mollis.",
-  },
-};
-const imageGalleryProps: ImageGalleryProps = {
-  post: {
-    imgPaths: [image1, image1, image1],
-    imgAlts: ["alt text!", "sadfsf", "asdfas"],
-    captions: ["this is a caption!!", "asefasdfsadf", "asfasdfasdfadsf"],
-  },
-};
-
-const imageFullProps: ImageFullProps = {
-  post: {
-    imgPath: image1,
-    imgAlt: "asdfasdfasdfsdfsdf",
-    caption: "asdfsdfasfsdfasdfsdfsdfdsfdsfsdfsdf sdfsd",
-  },
-};
+});
 
 ReactDOM.render(
   <React.Fragment>
-    <CssBaseline />
-    <TypographyPage />
-    {/* <Album /> */}
-    {/* <PostHeader post={postHeaderProps.post} /> */}
-    {/* <TextSection post={textSectionProps.post} /> */}
-    {/* <TextImageSection post={textImageSectionProps.post} /> */}
-    {/* <ImageGallery post={imageGalleryProps.post} /> */}
-    {/* <ImageFull post={imageFullProps.post} /> */}
+    {/* <CssBaseline /> */}
+    <ThemeProvider theme={theme}>
+      <TypographyPage />
+      {/* <Album /> */}
+      {/* <PostHeader post={postHeaderProps.post} /> */}
+      {/* <TextSection post={textSectionProps.post} /> */}
+      {/* <TextImageSection post={textImageSectionProps.post} /> */}
+      {/* <ImageGallery post={imageGalleryProps.post} /> */}
+      {/* <ImageFull post={imageFullProps.post} /> */}
+    </ThemeProvider>
   </React.Fragment>,
   document.getElementById("root")
 );
--- src/pages/type.tsx
+++ src/pages/type.tsx
@@ -2,15 +2,12 @@ import PostHeader from "../PostHeader";
 import TextSection from "../layouts/TextSection";
 import "../styles.css";
 import ImageGallery from "../layouts/ImageGallery";
-import ImageFull from "../layouts/ImageFull";
 import {
   typeHeaderProps,
   systemTextSection,
   systemGallery,
   mahlerTextImageSection,
-  mahlerFullImage,
   rubyTextImageSection,
-  rubyFullImage,
   rubyProgressGallery,
   rubyWorkGallery,
 } from "../page-content/type-content";
@@ -19,16 +16,15 @@ import TextImageSection from "../layouts/TextImageSection";
 export default function TypographyPage() {
   return (
     <div>
+      {/* page header */}
       <PostHeader post={typeHeaderProps.post} />
       {/* system */}
       <TextSection post={systemTextSection.post} />
       <ImageGallery post={systemGallery.post} />
       {/* mahler */}
       <TextImageSection post={mahlerTextImageSection.post} />
-      {/* <ImageFull post={mahlerFullImage.post} /> */}
       {/* ruby */}
       <TextImageSection post={rubyTextImageSection.post} />
-      {/* <ImageFull post={rubyFullImage.post} /> */}
       <ImageGallery post={rubyWorkGallery.post} />
       <ImageGallery post={rubyProgressGallery.post} />
     </div>
